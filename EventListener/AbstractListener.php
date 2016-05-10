f1a81c24511d1d7ab3b0fb898bd14da3463e6e8f
Philip Iezzi <github@onlime.ch>
2016-05-10 09:48:42 +0200

    made email reporting more robust by silently ignoring any exceptions and writing to log instead

--- EventListener/AbstractListener.php
+++ EventListener/AbstractListener.php
@@ -3,28 +3,48 @@
 namespace Onlime\ExceptionReportBundle\EventListener;
 
 use Onlime\ExceptionReportBundle\Utils\EmailReport;
+use Psr\Log\LoggerInterface;
 
 abstract class AbstractListener
 {
     /**
      * @var EmailReport
      */
-    protected $emailReport;
+    private $emailReport;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * @param EmailReport $emailReport
+     * @param LoggerInterface $logger
      */
-    public function __construct(EmailReport $emailReport)
+    public function __construct(EmailReport $emailReport, LoggerInterface $logger)
     {
-        $this->emailReport        = $emailReport;
+        $this->emailReport = $emailReport;
+        $this->logger      = $logger;
     }
 
     /**
+     * Send exception report email.
+     *
      * @param \Exception $exception
      * @param string|null $message
      */
     protected function sendEmailOnError(\Exception $exception, $message = null)
     {
-        $this->emailReport->send($exception, $message);
+        try {
+            $this->emailReport->send($exception, $message);
+        } catch (\Exception $e) {
+            // silently fail if exception report could not be sent
+            $this->logger->error(sprintf(
+                'Could not send exception report on %s%s: %s',
+                get_class($exception),
+                (null === $message) ? '' : " ($message)",
+                $e->getMessage()
+            ));
+        }
     }
 }
